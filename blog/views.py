6608074f1a8f102dbb54140e9a304137f2d26da1
root <arisaem7@naver.com>
2022-08-17 15:03:18 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
-
 
 class PostList(ListView):
     model = Post
@@ -13,6 +13,17 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
@@ -31,16 +42,17 @@ def category_page(request, slug):
             'category': category,
         }
     )
+
+
 class PostDetail(DetailView):
     model = Post
-
 
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
-
+# Create your views here.
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
